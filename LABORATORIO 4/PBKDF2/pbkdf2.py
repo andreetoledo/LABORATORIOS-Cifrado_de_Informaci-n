119b020d0b961400a21b86a92a8da15e2f3c532e
Edgar Andreé Toledo Torres <edgar.andree20@gmail.com>
2020-08-23 18:26:42 -0600

    pbkdf2 algoritmo

---
+++ LABORATORIO 4/PBKDF2/pbkdf2.py
@@ -0,0 +1,131 @@
+#Laboratorio 4 Cifrado
+
+#    : copyright: (c) Copyright 2011 de Armin Ronacher.
+#   : licencia: BSD, consulte LICENCIA para obtener más detalles.
+# -*- coding: utf-8 -*-
+
+"""
+  Este módulo implementa pbkdf2 para Python. También tiene algunos básicos
+    pruebas que aseguran que funciona. La implementación es sencilla
+    y usa solo cosas de stdlib y se puede copiar / pegar fácilmente en
+    tu aplicación favorita.
+
+    Use esto como reemplazo de bcrypt que no necesita una implementación de c
+    de un algoritmo criptográfico de pez globo modificado.
+
+    Uso de ejemplo:
+
+    >>> pbkdf2_hex ('lo que quiero hash', 'la sal aleatoria')
+    'fa7cc8a2b0a932f8e6ea42f9787e9d36e592e0c222ada6a9'
+
+    Como usar esto:
+
+    1. Utilice una función de comparación de cadenas de tiempo constante para comparar el hash almacenado
+        con el que estás generando:
+
+            def safe_str_cmp (a, b):
+                si len (a)! = len (b):
+                    falso retorno
+                rv = 0
+                para x, y en izip (a, b):
+                    rv | = ord (x) ^ ord (y)
+                return rv == 0
+
+    2. Utilice `os.urandom` para generar una sal adecuada de al menos 8 bytes.
+        Utilice una sal única por contraseña hash.
+
+    3. Almacene el `` algoritmo $ salt: costfactor $ hash '' en la base de datos para que
+        puede actualizar más tarde fácilmente a un algoritmo diferente si lo necesita
+        uno. Por ejemplo, `` PBKDF2-256 $ thesalt: 10000 $ deadbeef ... ''.
+
+
+
+"""
+import hmac
+import hashlib
+from struct import Struct
+from operator import xor
+from itertools import izip, starmap
+
+
+_pack_int = Struct('>I').pack
+
+
+def pbkdf2_hex(data, salt, iterations=1000, keylen=24, hashfunc=None):
+    """Like :func:`pbkdf2_bin` but returns a hex encoded string."""
+    return pbkdf2_bin(data, salt, iterations, keylen, hashfunc).encode('hex')
+
+
+def pbkdf2_bin(data, salt, iterations=1000, keylen=24, hashfunc=None):
+    """Returns a binary digest for the PBKDF2 hash algorithm of `data`
+    with the given `salt`.  It iterates `iterations` time and produces a
+    key of `keylen` bytes.  By default SHA-1 is used as hash function,
+    a different hashlib `hashfunc` can be provided.
+    """
+    hashfunc = hashfunc or hashlib.sha1
+    mac = hmac.new(data, None, hashfunc)
+    def _pseudorandom(x, mac=mac):
+        h = mac.copy()
+        h.update(x)
+        return map(ord, h.digest())
+    buf = []
+    for block in xrange(1, -(-keylen // mac.digest_size) + 1):
+        rv = u = _pseudorandom(salt + _pack_int(block))
+        for i in xrange(iterations - 1):
+            u = _pseudorandom(''.join(map(chr, u)))
+            rv = starmap(xor, izip(rv, u))
+        buf.extend(rv)
+    return ''.join(map(chr, buf))[:keylen]
+
+
+def test():
+    failed = []
+    def check(data, salt, iterations, keylen, expected):
+        rv = pbkdf2_hex(data, salt, iterations, keylen)
+        if rv != expected:
+            print 'Test failed:'
+            print '  Expected:   %s' % expected
+            print '  Got:        %s' % rv
+            print '  Parameters:'
+            print '    data=%s' % data
+            print '    salt=%s' % salt
+            print '    iterations=%d' % iterations
+            print
+            failed.append(1)
+
+    # From RFC 6070
+    check('password', 'salt', 1, 20,
+          '0c60c80f961f0e71f3a9b524af6012062fe037a6')
+    check('password', 'salt', 2, 20,
+          'ea6c014dc72d6f8ccd1ed92ace1d41f0d8de8957')
+    check('password', 'salt', 4096, 20,
+          '4b007901b765489abead49d926f721d065a429c1')
+    check('passwordPASSWORDpassword', 'saltSALTsaltSALTsaltSALTsaltSALTsalt',
+          4096, 25, '3d2eec4fe41c849b80c8d83662c0e44a8b291a964cf2f07038')
+    check('pass\x00word', 'sa\x00lt', 4096, 16,
+          '56fa6aa75548099dcc37d7f03425e0c3')
+    # This one is from the RFC but it just takes for ages
+    ##check('password', 'salt', 16777216, 20,
+    ##      'eefe3d61cd4da4e4e9945b3d6ba2158c2634e984')
+
+    # From Crypt-PBKDF2
+    check('password', 'ATHENA.MIT.EDUraeburn', 1, 16,
+          'cdedb5281bb2f801565a1122b2563515')
+    check('password', 'ATHENA.MIT.EDUraeburn', 1, 32,
+          'cdedb5281bb2f801565a1122b25635150ad1f7a04bb9f3a333ecc0e2e1f70837')
+    check('password', 'ATHENA.MIT.EDUraeburn', 2, 16,
+          '01dbee7f4a9e243e988b62c73cda935d')
+    check('password', 'ATHENA.MIT.EDUraeburn', 2, 32,
+          '01dbee7f4a9e243e988b62c73cda935da05378b93244ec8f48a99e61ad799d86')
+    check('password', 'ATHENA.MIT.EDUraeburn', 1200, 32,
+          '5c08eb61fdf71e4e4ec3cf6ba1f5512ba7e52ddbc5e5142f708a31e2e62b1e13')
+    check('X' * 64, 'pass phrase equals block size', 1200, 32,
+          '139c30c0966bc32ba55fdbf212530ac9c5ec59f1a452f5cc9ad940fea0598ed1')
+    check('X' * 65, 'pass phrase exceeds block size', 1200, 32,
+          '9ccad6d468770cd51b10e6a68721be611a8b4d282601db3b36be9246915ec82a')
+
+    raise SystemExit(bool(failed))
+
+
+if __name__ == '__main__':
+    test()
